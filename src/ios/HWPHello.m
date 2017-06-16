05a69f9925749586cb5b07b8abf65ad0bcab09e5
Oleg Ruyantsev <techokill@gmail.com>
2017-06-17 00:10:16 +0700

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -19,15 +19,22 @@ static NSString *_cover;
 
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
     MPRemoteCommandCenter *commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
+    NSNumber *shouldScrub = [NSNumber numberWithBool:YES];
+    [[[MPRemoteCommandCenter sharedCommandCenter] changePlaybackPositionCommand]
+     performSelector:@selector(setCanBeControlledByScrubbing:) withObject:shouldScrub];
+    [commandCenter.changePlaybackPositionCommand addTarget:self action:@selector(onChangePlayback:)];
     [commandCenter.playCommand addTarget:self action:@selector(onPlay:)];
     [commandCenter.pauseCommand addTarget:self action:@selector(onPause:)];
     [commandCenter.nextTrackCommand addTarget:self action:@selector(onNextTrack:)];
     [commandCenter.previousTrackCommand addTarget:self action:@selector(onPreviousTrack:)];
+    [commandCenter.seekBackwardCommand addTarget:self action:@selector(onSeekBackwardTrack:)];
+    [commandCenter.seekForwardCommand addTarget:self action:@selector(onSeekForwardTrack:)];
 }
 
 - (void)initSong:(CDVInvokedUrlCommand*)command
 {
     callbackID = command.callbackId;
+    center = [MPNowPlayingInfoCenter defaultCenter];
 //    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 
     NSString *url = [command.arguments objectAtIndex:0];
@@ -39,8 +46,10 @@ static NSString *_cover;
 //    NSNumber *elapsed = [command.arguments objectAtIndex:6];
 
     NSURL *soundUrl = [[NSURL alloc] initWithString:url];
+    AVURLAsset* audioAsset = [AVURLAsset URLAssetWithURL:soundUrl options:nil];
     NSLog(@"initSong, %@", soundUrl);
     NSLog(@"Song title %@", title);
+    
 
     _artist = artist;
     _title = title;
@@ -53,10 +62,11 @@ static NSString *_cover;
 //    [songInfo setObject:album forKey:@"album"];
 //    [songInfo setObject:cover forKey:@"cover"];
 
-    self.audioItem = [AVPlayerItem playerItemWithURL:soundUrl];
+    self.audioItem = [AVPlayerItem playerItemWithAsset:audioAsset];
 //    self.audioPlayer = [AVPlayer playerWithPlayerItem:self.audioItem];
     self.audioPlayer = [[AVPlayer alloc] initWithPlayerItem:self.audioItem];
     self.audioPlayer.automaticallyWaitsToMinimizeStalling = false;
+    self.audioPlayer.allowsExternalPlayback = false;
 
     [[NSNotificationCenter defaultCenter]
     addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.audioItem];
@@ -78,7 +88,65 @@ static NSString *_cover;
 {
     NSLog(@"play, %@", _title);
     [self.audioPlayer play];
+    [self updateMusicControls:false];
+}
 
+- (void)pause:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"pause");
+    [self.audioPlayer pause];
+    [self updateMusicControls:true];
+}
+
+- (void)sendEvent:(NSString*)event
+{
+    NSLog(@"Event, %@", event);
+
+    NSDictionary *dict = @{@"type": event};
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options: 0 error: nil];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
+    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
+    [plresult setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult:plresult callbackId:callbackID];
+}
+
+- (void)onPlay:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"play"]; }
+- (void)onPause:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"pause"]; }
+- (void)onNextTrack:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"nextTrack"]; }
+- (void)onPreviousTrack:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"previousTrack"]; }
+
+- (void)onSeekBackwardTrack:(MPRemoteCommandHandlerStatus*)event {
+    [self sendEvent:@"seekBackwardTrack"];
+}
+
+- (void)onSeekForwardTrack:(MPRemoteCommandHandlerStatus*)event {
+    [self sendEvent:@"seekForwardTrack"];
+}
+
+- (MPRemoteCommandHandlerStatus)setCanBeControlledByScrubbing:(MPChangePlaybackPositionCommandEvent*)event {
+    return MPRemoteCommandHandlerStatusSuccess;
+}
+
+- (MPRemoteCommandHandlerStatus)onChangePlayback:(MPChangePlaybackPositionCommandEvent*)event {
+    NSLog(@"changePlaybackPosition to %f", event.positionTime);
+    CMTime seekTime = CMTimeMakeWithSeconds(event.positionTime, 1);
+    [self.audioPlayer seekToTime:seekTime];
+    return MPRemoteCommandHandlerStatusSuccess;
+}
+
+- (void)updateMusicControls:(BOOL)isPause {
+    CMTime audioDuration = self.audioPlayer.currentItem.asset.duration;
+    CMTime audioCurrentTime = self.audioPlayer.currentTime;
+    
+    float audioDurationSeconds = CMTimeGetSeconds(audioDuration);
+    float audioCurrentTimeSeconds = CMTimeGetSeconds(audioCurrentTime);
+    
+    NSString *duration = [[NSNumber numberWithFloat:audioDurationSeconds] stringValue];
+    NSString *elapsed = [[NSNumber numberWithFloat:audioCurrentTimeSeconds] stringValue];
+    
+//    NSLog(@"duration, %@, %@", duration, elapsed);
+    
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         UIImage *image = nil;
         // check whether cover path is present
@@ -111,7 +179,7 @@ static NSString *_cover;
             // default named "no-image"
             image = [UIImage imageNamed:@"no-image"];
         }
-
+        
         // check whether image is loaded
         CGImageRef cgref = [image CGImage];
         CIImage *cim = [image CIImage];
@@ -119,44 +187,19 @@ static NSString *_cover;
             dispatch_async(dispatch_get_main_queue(), ^{
                 if (NSClassFromString(@"MPNowPlayingInfoCenter")) {
                     MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage: image];
-                    MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
                     center.nowPlayingInfo = [NSDictionary dictionaryWithObjectsAndKeys:
                                              _artist, MPMediaItemPropertyArtist,
                                              _title, MPMediaItemPropertyTitle,
                                              _album, MPMediaItemPropertyAlbumTitle,
                                              artwork, MPMediaItemPropertyArtwork,
-//                                             duration, MPMediaItemPropertyPlaybackDuration,
-//                                             elapsed, MPNowPlayingInfoPropertyElapsedPlaybackTime,
-                                             [NSNumber numberWithFloat:1.0f], MPNowPlayingInfoPropertyPlaybackRate, nil];
+                                             duration, MPMediaItemPropertyPlaybackDuration,
+                                             elapsed, MPNowPlayingInfoPropertyElapsedPlaybackTime,
+                                             [NSNumber numberWithFloat:(isPause ? 0.0f : 1.0f)], MPNowPlayingInfoPropertyPlaybackRate, nil];
                 }
             });
         }
     });
 }
-
-- (void)pause:(CDVInvokedUrlCommand*)command
-{
-    NSLog(@"pause");
-    [self.audioPlayer pause];
-}
-
-- (void)sendEvent:(NSString*)event
-{
-    NSLog(@"Event, %@", event);
-
-    NSDictionary *dict = @{@"type": event};
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options: 0 error: nil];
-    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-
-    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
-    [plresult setKeepCallbackAsBool:YES];
-    [self.commandDelegate sendPluginResult:plresult callbackId:callbackID];
-}
-
-- (void)onPlay:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"play"]; }
-- (void)onPause:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"pause"]; }
-- (void)onNextTrack:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"nextTrack"]; }
-- (void)onPreviousTrack:(MPRemoteCommandHandlerStatus*)event { [self sendEvent:@"previousTrack"]; }
 
 -(void)dealloc {
     [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
