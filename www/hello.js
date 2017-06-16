56556f227f29d54f0bc4ccec469a58ca00dce531
Oleg Ruyantsev <techokill@gmail.com>
2017-06-16 15:37:46 +0700

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,13 +1,19 @@
 /*global cordova, module*/
 
 module.exports = {
-    initSong: function (url, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "initSong", [url]);
-    },
-    play: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "play");
-    },
-    pause: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "pause");
-    }
+  initSong: function(url, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Hello", "initSong", [url]);
+  },
+  play: function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Hello", "play");
+  },
+  pause: function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Hello", "pause");
+  },
+  receiveRemoteEvent: function(event) {
+    var ev = document.createEvent('HTMLEvents');
+    ev.remoteEvent = event;
+    ev.initEvent('remote-event', true, true, arguments);
+    document.dispatchEvent(ev);
+  }
 };
