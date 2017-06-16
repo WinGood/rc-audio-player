074a40f19c1c26ab6154e49181659d3fd8d892b3
Oleg Ruyantsev <techokill@gmail.com>
2017-06-16 15:24:15 +0700

    Update HWPHello.h

--- src/ios/HWPHello.h
+++ src/ios/HWPHello.h
@@ -1,12 +1,18 @@
 #import <Cordova/CDV.h>
-@import AVFoundation;
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <MediaPlayer/MPNowPlayingInfoCenter.h>
+#import <MediaPlayer/MPMediaItem.h>
 
-@interface HWPHello : CDVPlugin
+@interface HWPHello : CDVPlugin {
+    NSString *callbackId;
+}
 
 - (void) initSong:(CDVInvokedUrlCommand*)command;
 - (void) play:(CDVInvokedUrlCommand*)command;
 - (void) pause:(CDVInvokedUrlCommand*)command;
 
+@property (nonatomic, copy) NSString *callbackId;
 @property (strong, nonatomic) AVPlayerItem *audioItem;
 @property (strong, nonatomic) AVPlayer *audioPlayer;
 
