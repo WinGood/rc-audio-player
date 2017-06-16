cdb56091b1856e065a2fb8f5d3921d42088918e7
Oleg Ruyantsev <techokill@gmail.com>
2017-06-16 11:56:06 +0700

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -2,17 +2,48 @@
 
 @implementation HWPHello
 
-- (void)greet:(CDVInvokedUrlCommand*)command
+- (void)initSong:(CDVInvokedUrlCommand*)command
 {
 
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+    NSString* url = [[command arguments] objectAtIndex:0];
+    NSLog(@"initSong, %@", url);
+    
+    NSURL *soundUrl = [[NSURL alloc] initWithString:url];
+    self.audioItem = [AVPlayerItem playerItemWithURL:soundUrl];
+    self.audioPlayer = [AVPlayer playerWithPlayerItem:self.audioItem];
+    
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.audioItem];
+    
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self selector:@selector(playerItemStalled:) name:AVPlayerItemPlaybackStalledNotification object:self.audioItem];
 
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
+//    CDVPluginResult* result = [CDVPluginResult
+//                               resultWithStatus:CDVCommandStatus_OK
+//                               messageAsString:msg];
+//
+//    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
 
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+- (void)playerItemDidReachEnd:(NSNotification *)notification{
+    [self.audioItem seekToTime:kCMTimeZero];
+    NSLog(@"AudioEnded");
+//    [self sendEventWithName:@"AudioEnded" body:@{@"event": @"finished"}];
+}
+- (void)playerItemStalled:(NSNotification *)notification{
+    [self.audioPlayer play];
+}
+
+- (void)play:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"play");
+    [self.audioPlayer play];
+}
+
+- (void)pause:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"pause");
+    [self.audioPlayer pause];
 }
 
 @end
