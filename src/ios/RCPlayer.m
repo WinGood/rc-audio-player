008b59de6eebe4d826fab575372fa48dfe81ed7c
Oleg Ruyantsev <techokill@gmail.com>
2017-06-20 12:43:50 +0700

    Update RCPlayer.m

--- src/ios/RCPlayer.m
+++ src/ios/RCPlayer.m
@@ -10,6 +10,7 @@ static NSString *_cover;
 static NSNumber *_isLoop;
 static bool audioListenersApplied = false;
 static bool songIsLoaded = false;
+static bool songIsStopped = false;
 
 static bool readyToPlay = false;
 static AVURLAsset *readyToPlayAsset;
@@ -78,6 +79,7 @@ static bool needPlaySong = false;
     songIsLoaded = false;
     readyToPlay = false;
     needPlaySong = false;
+    songIsStopped = false;
     
     // Stop loading previous song if it exits
     if (self.audioPlayer.currentItem) {
@@ -212,22 +214,22 @@ static bool needPlaySong = false;
 {
     NSLog(@"stop");
     [self.audioPlayer pause];
+    [self unregisterAudioListeners];
     
     self.audioPlayer = nil;
     self.audioItem = nil;
+    songIsStopped = true;
     
-    center.nowPlayingInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                             @"", MPMediaItemPropertyArtist,
-                             @"", MPMediaItemPropertyTitle,
-                             @"", MPMediaItemPropertyAlbumTitle,
-                             [NSNumber numberWithFloat:0.0f], MPNowPlayingInfoPropertyPlaybackRate, nil];
+    center.nowPlayingInfo = nil;
 }
 
 - (void)sendRemoteControlEvent:(NSString*)event
 {
     NSLog(@"Event, %@", event);
     // Send event in JS env
-    [self sendDataToJS:@{@"event": event}];
+    if (songIsStopped == false) {
+     [self sendDataToJS:@{@"event": event}];
+    }
 }
 
 - (void)onPlay:(MPRemoteCommandHandlerStatus*)event { [self sendRemoteControlEvent:@"play"]; }
@@ -263,67 +265,69 @@ static bool needPlaySong = false;
 }
 
 - (void)updateMusicControls:(BOOL)isPause {
-    CMTime audioDuration = self.audioPlayer.currentItem.asset.duration;
-    CMTime audioCurrentTime = self.audioPlayer.currentTime;
-
-    int audioDurationSeconds = CMTimeGetSeconds(audioDuration);
-    int audioCurrentTimeSeconds = CMTimeGetSeconds(audioCurrentTime);
-
-    NSString *duration = [[NSNumber numberWithInteger:audioDurationSeconds] stringValue];
-    NSString *elapsed = [[NSNumber numberWithInteger:audioCurrentTimeSeconds] stringValue];
-
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        UIImage *image = nil;
-        // check whether cover path is present
-        if (![_cover isEqual: @""]) {
-            // cover is remote file
-            if ([_cover hasPrefix: @"http://"] || [_cover hasPrefix: @"https://"]) {
-                NSURL *imageURL = [NSURL URLWithString:_cover];
-                NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-                image = [UIImage imageWithData:imageData];
-            }
-            // cover is full path to local file
-            else if ([_cover hasPrefix: @"file://"]) {
-                NSString *fullPath = [_cover stringByReplacingOccurrencesOfString:@"file://" withString:@""];
-                BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
-                if (fileExists) {
-                    image = [[UIImage alloc] initWithContentsOfFile:fullPath];
+    if (songIsStopped == false) {
+        CMTime audioDuration = self.audioPlayer.currentItem.asset.duration;
+        CMTime audioCurrentTime = self.audioPlayer.currentTime;
+        
+        int audioDurationSeconds = CMTimeGetSeconds(audioDuration);
+        int audioCurrentTimeSeconds = CMTimeGetSeconds(audioCurrentTime);
+        
+        NSString *duration = [[NSNumber numberWithInteger:audioDurationSeconds] stringValue];
+        NSString *elapsed = [[NSNumber numberWithInteger:audioCurrentTimeSeconds] stringValue];
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            UIImage *image = nil;
+            // check whether cover path is present
+            if (![_cover isEqual: @""]) {
+                // cover is remote file
+                if ([_cover hasPrefix: @"http://"] || [_cover hasPrefix: @"https://"]) {
+                    NSURL *imageURL = [NSURL URLWithString:_cover];
+                    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+                    image = [UIImage imageWithData:imageData];
+                }
+                // cover is full path to local file
+                else if ([_cover hasPrefix: @"file://"]) {
+                    NSString *fullPath = [_cover stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+                    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
+                    if (fileExists) {
+                        image = [[UIImage alloc] initWithContentsOfFile:fullPath];
+                    }
+                }
+                // cover is relative path to local file
+                else {
+                    NSString *basePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                    NSString *fullPath = [NSString stringWithFormat:@"%@%@", basePath, _cover];
+                    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
+                    if (fileExists) {
+                        image = [UIImage imageNamed:fullPath];
+                    }
                 }
             }
-            // cover is relative path to local file
             else {
-                NSString *basePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-                NSString *fullPath = [NSString stringWithFormat:@"%@%@", basePath, _cover];
-                BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
-                if (fileExists) {
-                    image = [UIImage imageNamed:fullPath];
-                }
+                // default named "no-image"
+                image = [UIImage imageNamed:@"no-image"];
             }
-        }
-        else {
-            // default named "no-image"
-            image = [UIImage imageNamed:@"no-image"];
-        }
-
-        // check whether image is loaded
-        CGImageRef cgref = [image CGImage];
-        CIImage *cim = [image CIImage];
-        if (cim != nil || cgref != NULL) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (NSClassFromString(@"MPNowPlayingInfoCenter")) {
-                    MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage: image];
-                    center.nowPlayingInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                                             _artist, MPMediaItemPropertyArtist,
-                                             _title, MPMediaItemPropertyTitle,
-                                             _album, MPMediaItemPropertyAlbumTitle,
-                                             artwork, MPMediaItemPropertyArtwork,
-                                             duration, MPMediaItemPropertyPlaybackDuration,
-                                             elapsed, MPNowPlayingInfoPropertyElapsedPlaybackTime,
-                                             [NSNumber numberWithFloat:(isPause ? 0.0f : 1.0f)], MPNowPlayingInfoPropertyPlaybackRate, nil];
-                }
-            });
-        }
-    });
+            
+            // check whether image is loaded
+            CGImageRef cgref = [image CGImage];
+            CIImage *cim = [image CIImage];
+            if (cim != nil || cgref != NULL) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (NSClassFromString(@"MPNowPlayingInfoCenter")) {
+                        MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage: image];
+                        center.nowPlayingInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+                                                 _artist, MPMediaItemPropertyArtist,
+                                                 _title, MPMediaItemPropertyTitle,
+                                                 _album, MPMediaItemPropertyAlbumTitle,
+                                                 artwork, MPMediaItemPropertyArtwork,
+                                                 duration, MPMediaItemPropertyPlaybackDuration,
+                                                 elapsed, MPNowPlayingInfoPropertyElapsedPlaybackTime,
+                                                 [NSNumber numberWithFloat:(isPause ? 0.0f : 1.0f)], MPNowPlayingInfoPropertyPlaybackRate, nil];
+                    }
+                });
+            }
+        });
+    }
 }
 
 -(void)itemDidFinishPlaying:(NSNotification *) notification {
